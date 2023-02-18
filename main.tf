c65f9a0a7cfb84e59dab133e571440c45a5c6a3c
DevOps4Me Global <69827393+devops4mecode@users.noreply.github.com>
2023-02-18 14:17:11 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 module "atlantis" {
   source  = "ringanta/ec2-atlantis/aws"
   version = "1.0.0"
-  public_key          = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDI2lljsQwnugxxTgSETI1X2EDSCETNchn6fdatZ7Q2gcwjCItm/yBwFkjfezhcRHb1mCUJN4EfqTxODFZxJfKPNSS2bZXN9NuqQwOwu3BjLhJtPIO+HF+OZ9mzXSOjkAKcye7uEDjgciI7C3+P5pL1IvWHmNDRF5JxFrRfVuw9JEpk0Pha5qagd69TOo2nAP2nmS6GlnqBwJCcVgBVPqSSu1Qm9kkCx9dmIfG5h5zoLQHNuhrCwQCX1NSXQzxnnPdVwBvG8nmLcg2qdzj28jMS84BQDVj8PMvjA0vZbEtxJk7dzAuA2G2Pn6KAzcEWUvcxaiQDCKKgxOdDKitdhCNf najibradzuan@DevOps4Me-Global.local"
-  attach_admin_policy = true
+  public_key          = "X
 }
 
 resource "aws_s3_bucket" "terraform_state" {
